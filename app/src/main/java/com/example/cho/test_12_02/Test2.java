052d58f67f3068b73f3d6ccc6a12d7a0b5b6eacc
doodoo1213 <kimjooyoung@gmail.com>
2016-12-08 22:23:45 +0900

    Merge branch 'master' of C:\dev\samples\test_12_02 with conflicts.

--- app/src/main/java/com/example/cho/test_12_02/Test2.java
+++ app/src/main/java/com/example/cho/test_12_02/Test2.java
@@ -1,5 +1,7 @@
 package com.example.cho.test_12_02;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,25 +11,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.support.v4.app.*;
 
 /**
  * Created by User on 2016-11-15.
  */
 
-public class Test2  extends Fragment {
+public class Test2  extends Fragment implements View.OnClickListener{
     private Context CurrentObj = getActivity();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.mgz_main, container, false);
-        Button button = (Button)view.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view) {
-                Intent redirect = new Intent(getActivity(),Test5.class);
-                getActivity().startActivity(redirect);
-            }
-        });
+        ((Button) view.findViewById(R.id.button)).setOnClickListener(this);
         return view;
     }
-};
+    public void onClick(View view) {
+        Fragment fragment = new Fragment();
+        FragmentManager fragmentManager = getFragmentManager();
+    }
+
--- app/src/main/java/com/example/cho/test_12_02/Test5.java
+++ app/src/main/java/com/example/cho/test_12_02/Test5.java
@@ -3,6 +3,7 @@ package com.example.cho.test_12_02;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,15 +18,12 @@ public class Test5  extends Fragment {
 
     }
     Button btn5;
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
-        View view = inflater.inflate(R.layout.home_to_more,container,false);
-
-
-        return view;
-    }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            return inflater.inflate(R.layout.home_to_info, container, false);
+        }
 
 }
