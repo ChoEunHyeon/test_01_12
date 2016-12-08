b841f9ecfad957f163364968623c70efb702d437
doodoo1213 <kimjooyoung@gmail.com>
2016-12-08 21:50:37 +0900

    Test2_intent

--- app/src/main/java/com/example/cho/test_12_02/Test2.java
+++ app/src/main/java/com/example/cho/test_12_02/Test2.java
@@ -1,35 +1,33 @@
 package com.example.cho.test_12_02;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 /**
  * Created by User on 2016-11-15.
  */
 
 public class Test2  extends Fragment {
-    public Test2(){
-
-    }
-
+    private Context CurrentObj = getActivity();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        View view = inflater.inflate(R.layout.home_to_mgz_wv,container,false);
-      WebView webView = (WebView) view.findViewById(R.id.web_view);
-       webView.setWebViewClient(new WebViewClient());
-        webView.loadUrl("http://igbtkd.wixsite.com/mysite-2/2016");
-        webView.getSettings().setJavaScriptEnabled(true);
+        View view = inflater.inflate(R.layout.mgz_main, container, false);
+        Button button = (Button)view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent redirect = new Intent(getActivity(),Test5.class);
+                getActivity().startActivity(redirect);
+            }
+        });
         return view;
     }
-
-}
+};
