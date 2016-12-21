22850c3a0912d296f74d5bc732992d9bf7d18081
kunyoungP <p3145@daum.net>
2016-12-21 19:27:10 +0900

    12_21_7_26

--- app/src/main/java/com/example/cho/test_12_02/Test1.java
+++ app/src/main/java/com/example/cho/test_12_02/Test1.java
@@ -1,10 +1,12 @@
 package com.example.cho.test_12_02;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by User on 2016-11-15.
@@ -19,8 +21,33 @@ public class Test1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.home_to_info, container, false);
 
-        return inflater.inflate(R.layout.home_to_info, container, false);
+        Button button = (Button)view.findViewById(R.id.buttons_3);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent redirect = new Intent(getActivity(),Samuchc.class);
+                getActivity().startActivity(redirect);
+            }
+        });
+
+        Button button1 = (Button)view.findViewById(R.id.buttons_2);
+        button1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent redirect = new Intent(getActivity(),Supum.class);
+                getActivity().startActivity(redirect);
+            }
+        });
+
+        Button button2 = (Button)view.findViewById(R.id.buttons_4);
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                Intent redirect = new Intent(getActivity(),Yenmeangjang.class);
+                getActivity().startActivity(redirect);
+            }
+        });
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/example/cho/test_12_02/Yenmeangjang.java
@@ -0,0 +1,17 @@
+package com.example.cho.test_12_02;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 2016-12-21.
+ */
+
+public class Yenmeangjang extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yonmeang);
+    }
+}
