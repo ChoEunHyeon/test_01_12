161322ab2254f24c6c3995fac15941762cd5bbba
cho <igbtkd@naver.com>
2017-01-12 22:01:54 +0900

    폴더 추가

--- app/src/main/java/yeom/example/cho/test_12_02/Group.java
+++ app/src/main/java/yeom/example/cho/test_12_02/Group.java
@@ -3,9 +3,11 @@ package yeom.example.cho.test_12_02;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 /**
@@ -30,5 +32,52 @@ public class Group extends Fragment{
         });
 
         return view;
+    }
+
+    /**
+     * Created by Samsung on 2016-12-09.
+     */
+
+    public static class CMS extends Fragment {
+
+        View view;
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            view = inflater.inflate(R.layout.cms_scroll, container, false);
+
+            ImageButton button;
+
+            button = (ImageButton) view.findViewById(R.id.imageButton_cms);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                  //AlertDialogCreate();
+                    FragmentManager fm = getFragmentManager();
+                    CMS_dialog dialogFragment = new CMS_dialog ();
+                //    dialogFragment.getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+                    dialogFragment.show(fm,"m");
+                }
+            });
+
+
+
+            return view;
+        }
+
+
+    /*
+        public void AlertDialogCreate() {
+
+            new AlertDialog.Builder(getActivity())
+                    .setIcon(R.mipmap.ic_launcher)
+                    .setTitle("Alert Dialog Box Title")
+                    .setMessage("Are you sure( Alert Dialog Message )")
+                    .setPositiveButton("OK", null)
+                    .setNegativeButton("Cancel", null);
+
+        }*/
     }
 }
--- app/src/main/java/yeom/example/cho/test_12_02/Test4.java
+++ app/src/main/java/yeom/example/cho/test_12_02/Test4.java
@@ -10,8 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import yeom.example.cho.test_12_02.a.CMS;
-
 
 public class Test4 extends AppCompatActivity {
 
@@ -39,7 +37,7 @@ public class Test4 extends AppCompatActivity {
         magazine_viewPagerAdapter.addFragments(new Student(), getResources().getString(R.string.mgz_tab_student));
         magazine_viewPagerAdapter.addFragments(new Instructor(), getResources().getString(R.string.mgz_tab_instructor));
         magazine_viewPagerAdapter.addFragments(new Group(), getResources().getString(R.string.mgz_tab_group));
-        magazine_viewPagerAdapter.addFragments(new CMS(), getResources().getString(R.string.mgz_tab_CMS));
+        magazine_viewPagerAdapter.addFragments(new Group.CMS(), getResources().getString(R.string.mgz_tab_CMS));
         magazine_viewPager.setAdapter(magazine_viewPagerAdapter);
         magazine_tabLayout.setupWithViewPager(magazine_viewPager);
         magazine_viewPager.setCurrentItem(0, true);
